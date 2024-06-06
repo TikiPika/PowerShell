a724e35f84574ebb2674bccca221a39cc75a0c5b
TikiPika <77382240+TikiPika@users.noreply.github.com>
2024-06-06 09:56:52 +0800

    Update log.ps1

--- log.ps1
+++ log.ps1
@@ -1,3 +1,12 @@
+# 提升权限运行
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+
+
+
 New-PSDrive -Name "X" -PSProvider FileSystem -Root "\\server\share" -Persist
 
 # 检查计划任务是否存在
